947cc4620c8d7e7604e3720a9d5b3aef86732cee
Everton Souza <evrasouza@gmail.com>
2022-07-06 07:11:47 -0300

    Primeiro Teste Cypress

---
+++ training-wheels/cypress/integration/home.spec.js
@@ -0,0 +1,11 @@
+
+
+it('deve exibir a home page', function () {
+    cy.visit('https://training-wheels-qaninja.herokuapp.com/')
+
+    cy.title()
+        .should('eq', 'Training Wheels | QAninja')
+
+    cy.get('ul[class=menu-list]')
+        .should('be.visible')
+})
