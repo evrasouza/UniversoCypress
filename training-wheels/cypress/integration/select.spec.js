64c38ad7a18003f5ca11533195e197bdb69f5d99
Everton Souza <evrasouza@gmail.com>
2022-07-06 18:54:58 -0300

    Manipulando Select (combo-box)

---
+++ training-wheels/cypress/integration/select.spec.js
@@ -0,0 +1,17 @@
+
+
+it('selecionar bucky por id', function () {
+    cy.visit('/select')
+
+    cy.get('#avengerList')
+        .select('Bucky')
+})
+
+it('selecionar tony stark sem ser pelo id', function () {
+    cy.visit('/select')
+
+    cy.contains('option', 'Selecione um personagem')
+        .parent()
+        .select('Tony Stark')
+        .should('have.value', '3')
+})
