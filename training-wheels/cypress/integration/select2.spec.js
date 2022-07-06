e5fc48ff99217937999c9c4f4aa174c30807d9c3
Everton Souza <evrasouza@gmail.com>
2022-07-06 19:52:34 -0300

    Manipulando Select 2

---
+++ training-wheels/cypress/integration/select2.spec.js
@@ -0,0 +1,8 @@
+
+
+it('deve selecionar uma unica opcao', function () {
+    cy.visit('/apps/select2/index.html')
+
+    cy.get('.select2-selection--single').click()
+    cy.contains('.select2-results__option', 'Cypress').click()
+})
