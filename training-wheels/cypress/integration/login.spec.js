22a6a0ad8020add4d934344797db965912798a1d
Everton Souza <evrasouza@gmail.com>
2022-07-06 08:59:42 -0300

    Validando textos

--- training-wheels/cypress/integration/login.spec.js
+++ training-wheels/cypress/integration/login.spec.js
@@ -10,4 +10,12 @@ it('deve exibir a home page', function () {
         .type('pwd123')
     
     cy.get('button[type=submit]').click()
+
+    cy.get('h3[class^=title]')
+        .should('have.text', 'Olá Papito, bem-vindo ao Orkut')
+
+    const expected_text = 'Em breve você poderá participar de comunidades, adicionar amigos e deixar um Scraps. hahahahah'
+
+    cy.get('p[class=subheader]')
+        .should('have.text', expected_text)
 })
