75b8eb52554c3259edc44c0f6bdfff8f2a805640
Everton Souza <evrasouza@gmail.com>
2022-07-07 14:18:46 -0300

    Javascript Confirm Popup

--- training-wheels/cypress/integration/alerts.spec.js
+++ training-wheels/cypress/integration/alerts.spec.js
@@ -1,7 +1,7 @@
 
 
 
-it('deve marcar os top 5 filmes da marvel', function () {
+it('deve exibir uma mensagem de alerta', function () {
     cy.visit('/javascript_alerts')
 
         cy.contains('button', 'Alerta')
@@ -9,5 +9,29 @@ it('deve marcar os top 5 filmes da marvel', function () {
         cy.on('window:alert', function(result){
             expect(result).to.equal('Isto é uma mensagem de alerta')
         })
+
+})
+
+
+
+it('deve confirmar a solicitacao', function () {
+    cy.visit('/javascript_alerts')
+
+        cy.contains('button', 'Confirma')
+            .click()
+        cy.on('window:confirm', () => true)
+
+        cy.get('#result').should('have.text', 'Mensagem confirmada')
+
+})
+
+it('deve cancelar a solicitacao', function () {
+    cy.visit('/javascript_alerts')
+
+        cy.contains('button', 'Confirma')
+            .click()
+        cy.on('window:confirm', () => false)
+
+        cy.get('#result').should('have.text', 'Mensagem não confirmada')
 
 })
