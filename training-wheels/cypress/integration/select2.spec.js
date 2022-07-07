5e65afbe82959215c17be7fe1e0506fadc401e1b
Everton Souza <evrasouza@gmail.com>
2022-07-07 06:15:32 -0300

    Manipulando Select 2 multiplo

--- training-wheels/cypress/integration/select2.spec.js
+++ training-wheels/cypress/integration/select2.spec.js
@@ -6,3 +6,19 @@ it('deve selecionar uma unica opcao', function () {
     cy.get('.select2-selection--single').click()
     cy.contains('.select2-results__option', 'Cypress').click()
 })
+
+
+it('deve selecionar multiplas opcoes', function () {
+    cy.visit('/apps/select2/index.html')
+
+    const framework = [
+        'Cypress',
+        'Robot Framework',
+        'Protractor'
+    ]
+
+    framework.forEach(function(framework){
+        cy.get('.select2-selection--multiple').click()
+        cy.contains('.select2-results__option', framework).click()
+    })    
+})
