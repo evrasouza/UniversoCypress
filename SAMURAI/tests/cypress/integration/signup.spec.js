e0f0fdd4d28a08fd3f81dd86e938fcb6f60a5810
Everton Souza <evrasouza@gmail.com>
2022-07-12 09:02:06 -0300

    Criar uma task que volta no tempo

--- SAMURAI/tests/cypress/integration/signup.spec.js
+++ SAMURAI/tests/cypress/integration/signup.spec.js
@@ -6,6 +6,11 @@ it('deve cadastrar um novo usuário', function () {
     const name = 'Everton Souza'
     const email = 'evrasouza@samuraibs.com.br'
     const password = 'pwd123'
+
+    cy.task('removeUser', email)
+        .then(function(result){
+            console.log(result)
+        })
     
     cy.visit('/signup')
 
@@ -13,13 +18,13 @@ it('deve cadastrar um novo usuário', function () {
     cy.get('input[placeholder="E-mail"]').type(email)
     cy.get('input[placeholder="Senha"]').type(password)
 
-    cy.intercept('POST', '/users', {
-        statusCode: 200
-    }).as('postUser')
+    // cy.intercept('POST', '/users', {
+    //     statusCode: 200
+    // }).as('postUser')
 
     cy.contains('button', 'Cadastrar').click()
 
-    cy.wait('@postUser')
+    // cy.wait('@postUser')
 
     cy.get('.toast')
         .should('be.visible')
