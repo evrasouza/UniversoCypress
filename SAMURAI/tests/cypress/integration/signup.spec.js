df109356ebf8f09d0d37b1a41b1e05512c12d3ee
Everton Souza <evrasouza@gmail.com>
2022-07-12 14:04:29 -0300

    O segredo da automação de testes

--- SAMURAI/tests/cypress/integration/signup.spec.js
+++ SAMURAI/tests/cypress/integration/signup.spec.js
@@ -1,13 +1,13 @@
 
 describe('Cadastro', function () {
 
-    const user = {
-        name: 'Everton Souza',
-        email: 'evrasouza@samuraibs.com.br',
-        password: 'pwd123'
-    }
-
     it('deve cadastrar um novo usuário', function () {
+
+        const user = {
+            name: 'Everton Souza',
+            email: 'evrasouza@samuraibs.com.br',
+            password: 'pwd123'
+        }
 
         cy.task('removeUser', user.email)
             .then(function (result) {
@@ -20,13 +20,7 @@ describe('Cadastro', function () {
         cy.get('input[placeholder="E-mail"]').type(user.email)
         cy.get('input[placeholder="Senha"]').type(user.password)
 
-        // cy.intercept('POST', '/users', {
-        //     statusCode: 200
-        // }).as('postUser')
-
         cy.contains('button', 'Cadastrar').click()
-
-        // cy.wait('@postUser')
 
         cy.get('.toast')
             .should('be.visible')
@@ -34,7 +28,27 @@ describe('Cadastro', function () {
             .should('have.text', 'Agora você se tornou um(a) Samurai, faça seu login para ver seus agendamentos!')
     })
 
-    it('deve exibir email já cadastrado', function () {
+    it.only('deve exibir email já cadastrado', function () {
+
+        const user = {
+            name: 'Dave Murray',
+            email: 'davemurray@samuraibs.com.br',
+            password: 'pwd123',
+            is_provider: true
+        }
+
+        cy.task('removeUser', user.email)
+        .then(function (result) {
+            console.log(result)
+        })
+
+        cy.request(
+            'POST',
+            'http://localhost:3333/users',
+            user
+            ).then(function(response) {
+                expect(response.status).to.eql(200)
+            })
 
         cy.visit('/signup')
 
