ce6abb59229eed8002f79dca5e9c363e7a8543bc
Everton Souza <evrasouza@gmail.com>
2022-07-13 19:31:14 -0300

    O Dev estragou minha massa. E agora?

--- SAMURAI/tests/cypress/integration/login.spec.js
+++ SAMURAI/tests/cypress/integration/login.spec.js
@@ -1,23 +1,39 @@
 import loginPage from '../support/pages/login'
 import dashPage from '../support/pages/dashboard'
 
-describe('login', function(){
+describe('login', function () {
 
-    context('quando o usuário é muito bom', function(){
+    context('quando o usuário é muito bom', function () {
 
         const user = {
             name: "avatar",
             email: "avatar@samuraibs.com.br",
-            password: "pwd123"
+            password: "pwd123",
+            is_provider: true
         }
 
-        it('deve logar com sucesso', function(){
+        before(function () {
+            cy.task('removeUser', user.email)
+                .then(function (result) {
+                    console.log(result)
+                })
+
+            cy.request(
+                'POST',
+                'http://localhost:3333/users',
+                user
+            ).then(function (response) {
+                expect(response.status).to.eql(200)
+            })
+        })
+
+        it('deve logar com sucesso', function () {
             loginPage.go()
             loginPage.form(user)
             loginPage.submit()
 
             dashPage.header.userLoggedIn(user.name)
-        })        
+        })
 
     })
 
