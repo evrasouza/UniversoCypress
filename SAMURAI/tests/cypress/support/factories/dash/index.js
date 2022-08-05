c14d9ac0cb124eaf93af334f1fda507c156dd98d
Everton Souza <evrasouza@gmail.com>
2022-08-05 18:22:38 -0300

    Massa de testes dinâmica com Underscore

--- SAMURAI/tests/cypress/support/factories/dash/index.js
+++ SAMURAI/tests/cypress/support/factories/dash/index.js
@@ -1,5 +1,5 @@
 
-
+import _ from 'underscore'
 
 exports.customer = {
     name: 'Nikki Sixx',
@@ -16,5 +16,5 @@ exports.provider = {
 }
 
 exports.appointment = {
-    hour: '14:00'    
+    hour: _.sample(['08:00', '09:00', '10:00', '11:00', '12:00', '13:00', '14:00', '15:00', '16:00', '17:00'])
 }
